28fdb67f12c66bfe4f806dc3156e144496b7dbed
dhlee <dhlee@emro.co..kr>
2020-12-04 09:11:41 +0900

    Overrding Test 추가

---
+++ src/main/java/dhlee/apiclass/overriding/VIPCustomer.java
@@ -0,0 +1,29 @@
+package dhlee.apiclass.overriding;
+
+public class VIPCustomer extends Customer{
+
+    double salesRatio;
+    private int agentID;
+
+    /*
+    public VIPCustomer() {
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+    }
+    */
+
+    public VIPCustomer(int customerID, String customerName) {
+        super(customerID, customerName);
+
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        return price - (int)(price * salesRatio);
+    }
+}
