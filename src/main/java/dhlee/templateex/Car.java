b2709322230732e2e368cd653151483347438e1a
dhlee <dhlee@emro.co..kr>
2020-12-08 09:11:13 +0900

    abstract template method Test

---
+++ src/main/java/dhlee/templateex/Car.java
@@ -0,0 +1,23 @@
+package dhlee.templateex;
+
+public abstract class Car {
+    public abstract void drive();
+    public abstract void stop();
+
+    public void startCar() {
+        System.out.println("시동을 켭니다.");
+    }
+
+    public void turnOff() {
+        System.out.println("시동을 끕니다.");
+    }
+
+    // 템플릿 메서드
+    // 시나리오는 변하면 안됨 : 하위 클래스에서 재정의 할 수 없게 하기 위해 final 사용
+    final public void run() {
+        startCar();
+        drive();
+        stop();
+        turnOff();
+    }
+}
---
+++ src/main/java/dhlee/templateex/CarTest.java
@@ -0,0 +1,12 @@
+package dhlee.templateex;
+
+public class CarTest {
+    public static void main(String[] args) {
+        AICar aiCar = new AICar();
+        aiCar.run();
+
+        System.out.println("=====================");
+        Car manualCar = new ManualCar();
+        manualCar.run();
+    }
+}
