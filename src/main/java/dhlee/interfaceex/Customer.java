88128a6c682c41492266dcc1051eba4842d3dd6a
dhlee <dhlee@emro.co..kr>
2020-12-10 09:56:48 +0900

    인터페이스 다중 상속 및 중복 메서드 재정의

---
+++ src/main/java/dhlee/interfaceex/Customer.java
@@ -0,0 +1,22 @@
+package dhlee.interfaceex;
+
+public class Customer implements Buy, Sell{
+    @Override
+    public void buy() {
+        System.out.println("Customer Buy");
+    }
+
+    @Override
+    public void sell() {
+        System.out.println("Customer Sell");
+    }
+
+    @Override
+    public void order() {
+        System.out.println("Customer Order");
+    }
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
