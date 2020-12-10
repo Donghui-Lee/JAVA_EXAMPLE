737c27269908c68af903d3e6d49976aa991937a0
dhlee <dhlee@emro.co..kr>
2020-12-10 09:40:46 +0900

    인터페이스 요소(Default Method, Static Method, Private Method) 추가

--- src/main/java/dhlee/interfaceex/Calc.java
+++ src/main/java/dhlee/interfaceex/Calc.java
@@ -8,4 +8,32 @@ public interface Calc {
     int substract(int num1, int num2);
     int times(int num1, int num2);
     int divide(int num1, int num2);
+
+    // Defalt Method
+    default void description() {
+        System.out.println("정수 계산기를 구현합니다.");
+        //myMethod();
+    }
+
+    // Static Method
+    static int total(int[] arr) {
+        //myStaticMethod();
+        int total = 0;
+
+        for(int i : arr) {
+            total += i;
+        }
+        return total;
+    }
+
+    // Private Method (일반 Method)
+    // Java 9 부터 제공
+    //private void myMethod() {
+    //    System.out.println("private method");
+    //}
+    // Private Method (Static Method)
+    //private static void myStaticMethod() {
+    //    System.out.println("private static method");
+    //}
+
 }
--- src/main/java/dhlee/interfaceex/CalcTest.java
+++ src/main/java/dhlee/interfaceex/CalcTest.java
@@ -11,5 +11,12 @@ public class CalcTest {
         System.out.println(completeCalc.times(n1, n2));
         System.out.println(completeCalc.divide(n1, n2));
         completeCalc.showInfo();
+
+        // Default Method 사용
+        completeCalc.description();
+
+        // Static Method 사용(인스턴스 변수로 가져다 사용하는 것이 아니라 인터페이스 타입으로 가져다 사용)
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Calc.total(arr));
     }
 }
